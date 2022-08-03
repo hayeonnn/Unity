2ee5069c81d36ebc207541cd06cf1e2c03aab56f
Rubbe1124 <nbj10@naver.com>
2022-08-03 23:28:08 +0900

    8/3
    ===슬라임 1페이즈===
    (플레이어가 있는 위치에 산성 용액을 한 방씩 뱉는 패턴) 완료

--- Assets/Rubbe/Scripts/Charcter/BossController.cs
+++ Assets/Rubbe/Scripts/Charcter/BossController.cs
@@ -9,20 +9,28 @@ public class BossController : MonoBehaviour
     [SerializeField] Transform m_tfArrow = null;
 
     public GameObject MainChar;
+    public Vector2 t_MainCharacter;
 
     // Start is called before the first frame update
     void Start()
     {
-
+        InvokeRepeating("TryFire", 1f, 1f);
     }
 
     void LookAtMainCharacter()
     {
-        Vector2 t_MainCharacter = MainChar.transform.position;
+        t_MainCharacter = MainChar.transform.position;
         Vector2 t_direction = new Vector2(t_MainCharacter.x - m_tfArrow.position.x,
                                         t_MainCharacter.y - m_tfArrow.position.y);
 
         m_tfArrow.right = t_direction;
+    }
+
+    void TryFire()
+    {
+        float distance = Vector2.Distance(t_MainCharacter, transform.position);
+        GameObject t_arrow = Instantiate(m_goPrefab, m_tfArrow.position, m_tfArrow.rotation);
+        t_arrow.GetComponent<Rigidbody2D>().velocity = (t_arrow.transform.right * 8f * distance / 8) + (t_arrow.transform.up * -2f * distance / 5);
     }
 
     // Update is called once per frame
