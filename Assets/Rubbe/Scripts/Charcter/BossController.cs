c8e97bf3cdd878127a132511dbe7c2a5c5194afe
Rubbe1124 <nbj10@naver.com>
2022-08-04 22:06:20 +0900

    8/4
    ===보스 슬라임 페이즈1===
    산성액 발사 완료
    점프 공격 완료
    
    ===보스 슬라임 페이즈2===
    점프후 공격, 튕기기 완료

--- Assets/Rubbe/Scripts/Charcter/BossController.cs
+++ Assets/Rubbe/Scripts/Charcter/BossController.cs
@@ -11,10 +11,12 @@ public class BossController : MonoBehaviour
     public GameObject MainChar;
     public Vector2 t_MainCharacter;
 
+    public int boss_Phase = 1;
+
     // Start is called before the first frame update
     void Start()
     {
-        InvokeRepeating("TryFire", 1f, 1f);
+        InvokeRepeating("BossPattern", 0.5f, 4f);
     }
 
     void LookAtMainCharacter()
@@ -26,11 +28,76 @@ public class BossController : MonoBehaviour
         m_tfArrow.right = t_direction;
     }
 
+    void BossPattern()
+    {
+        if (boss_Phase == 1)
+        {
+            int temp = Random.Range(1, 4);
+            if (temp == 1||temp==2)
+            {
+                Invoke("TryFire", 0.5f);
+                Invoke("TryFire", 1f);
+                Invoke("TryFire", 1.5f);
+                Invoke("TryFire", 2f);
+                Invoke("TryFire", 2.5f);
+            }
+            else if (temp == 3)
+            {
+                Invoke("JumpPhase1", 0.5f);
+            }
+        }
+        else if (boss_Phase == 2)
+        {
+            Invoke("JumpPhase2", 0.5f);
+        }
+    }
+
     void TryFire()
     {
         float distance = Vector2.Distance(t_MainCharacter, transform.position);
         GameObject t_arrow = Instantiate(m_goPrefab, m_tfArrow.position, m_tfArrow.rotation);
-        t_arrow.GetComponent<Rigidbody2D>().velocity = (t_arrow.transform.right * 8f * distance / 8) + (t_arrow.transform.up * -2f * distance / 5);
+        t_arrow.GetComponent<Rigidbody2D>().velocity = (t_arrow.transform.right * 9f * distance / 8) + (t_arrow.transform.up * -2f * distance / 5);
+    }
+
+    void JumpPhase1()
+    {
+        Invoke("JumpToSky", 0.1f);
+        Invoke("JumpToPlayer", 1f);
+        Invoke("ComeBack", 2.8f);
+    }
+
+    void JumpPhase2()
+    {
+        Invoke("JumpToSky", 0.1f);
+        Invoke("JumpToPlayer", 1f);
+        Invoke("JumpToPlayerAgain", 2.3f);
+        Invoke("ComeBack", 3.8f);
+    }
+
+    void JumpToSky()
+    {
+        transform.GetComponentInParent<Rigidbody2D>().velocity = transform.parent.up * 30f;
+    }
+
+    void JumpToPlayer()
+    {
+        transform.GetComponentInParent<Rigidbody2D>().velocity = transform.parent.up * -5f;
+        transform.parent.transform.position = MainChar.transform.position + (Vector3.up * 15f);
+    }
+
+    void JumpToPlayerAgain()
+    {
+        if (MainChar.transform.position.x - transform.parent.transform.position.x < 0)
+            transform.GetComponentInParent<Rigidbody2D>().velocity = Vector2.up * 5f + Vector2.left * 5f;
+        else
+            transform.GetComponentInParent<Rigidbody2D>().velocity = Vector2.up * 5f + Vector2.right * 5f;
+    }
+
+    void ComeBack()
+    {
+        transform.GetComponentInParent<Rigidbody2D>().velocity = Vector2.zero;
+        Vector3 temp=new Vector3(4.96000004f, -1.0032717f, 0);
+        transform.parent.transform.position = temp;
     }
 
     // Update is called once per frame
