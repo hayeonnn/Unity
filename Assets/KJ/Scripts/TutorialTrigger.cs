85232a81190ddea0e1e4accff6b9c3de5dac8e4e
RafesiA <whrudwls98@naver.com>
2022-08-18 20:42:32 +0900

    Feature: Tutorial Tip

--- Assets/KJ/Scripts/TutorialTrigger.cs
+++ Assets/KJ/Scripts/TutorialTrigger.cs
@@ -8,16 +8,36 @@ public class TutorialTrigger : MonoBehaviour
     public GameObject[] triggers;
     public Animator tutoAnim_1;
     public Animator tutoAnim_2;
+    public Stack<Animator> tutorialOrder;
+    public Animator currentAnimator;
 
     private void Awake() {
+        tutorialOrder = new Stack<Animator>();
 
+        tutorialOrder.Push(tutoAnim_2);
+        tutorialOrder.Push(tutoAnim_1);
     }
 
     private void OnTriggerEnter2D(Collider2D other) {
         if(other.tag == "Player"){
             Debug.Log("트리거 발동");
-            tutoAnim_1.SetBool("isOpen", true);
+            currentAnimator = tutorialOrder.Pop();
+
+            currentAnimator.SetBool("isOpen", true);
+
+            StartCoroutine("WaitTip");
+            
+
         }
         
+    }
+    
+    IEnumerator WaitTip(){
+
+        yield return new WaitForSeconds(2f);
+
+        currentAnimator.SetBool("isOpen", false);
+
+        StopCoroutine("WaitTip");
     }
 }
